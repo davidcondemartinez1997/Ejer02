fa9e81e316927bf209abf764f3f1d5e363ce93d5
dconde <davidcondemartinez1997@gmail.com>
2017-07-04 18:27:33 +0200

    Metodo de resta completado y testeado

--- Ejer02/Class1.cs
+++ Ejer02/Class1.cs
@@ -8,5 +8,9 @@ namespace Ejer02
 {
     public class Class1
     {
+        public int Resta(int A, int B)
+        {
+            return A - B;
+        }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Ejer02;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private Class1 sut;
+
+        [TestInitialize]
+        public void TestInicial()
+        {
+            sut = new Class1();
+        }
+
+        [TestMethod]
+        public void TestResta()
+        {
+            int res = sut.Resta(5, 2);
+
+            Assert.AreEqual(3, res);
+
+        }
+
+        [TestMethod]
+        public void TestRestaNegativos()
+        {
+            int res = sut.Resta(-5, -2);
+
+            Assert.AreEqual(-3, res);
+
+        }
+    }
+}
